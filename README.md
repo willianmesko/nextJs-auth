68c21717d00ad44da10ebbdf7198156df82feb59
Mesko <willianmesko@hotmail.com>
2021-08-01 18:45:44 -0300

    commit all

---
+++ README.md
@@ -0,0 +1,11 @@
+# Ignite - Trilha ReactJS: Chapter IV
+
+## Autenticação e autorização
+
+Processo completo de autenticação, autorização e refresh token dentro do React com Next.js, controle de acesso à rotas e componentes tanto no client-side quando server-side.
+
+<br>
+<br>
+<br>
+
+Desenvolvido com ❤️ por [Fernanda Leite](https://www.linkedin.com/in/fcleite19/)
---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import { FormEvent, useContext, useState } from 'react';
+
+import { AuthContext } from '../contexts/AuthContext';
+
+import styles from '../styles/Home.module.css';
+import { withSSRGuest } from '../utils/withSSRGuest';
+
+export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signIn } = useContext(AuthContext)
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password
+    }
+
+    await signIn(data)
+  }
+
+  return (
+    <div className={styles.container}>
+      <form  onSubmit={handleSubmit}>
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  )
+}
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+})
